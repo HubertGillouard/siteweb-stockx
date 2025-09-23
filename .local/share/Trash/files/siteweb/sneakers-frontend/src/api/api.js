fbce66c23348adf26617c7456168c07cf61e1ddf
Ashucn7 <max.chen@epitech.digital>
2025-09-23 15:37:11 +0200

    Supprime fichier incompatible Windows

--- .local/share/Trash/files/siteweb/sneakers-frontend/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({ 
-  baseURL: "http://localhost:4001" // <-- ton backend
-});
-
-export const getProducts = () => API.get("/products");
-export const getProduct = (id) => API.get(`/products/${id}`);
-export default API;
--- finalsite/frontend/src/services/api.js
+++
@@ -1,5 +0,0 @@
-export const getProducts = async () => {
-    const res = await fetch('http://localhost:3000/api/products');
-    return res.json();
-  };
-  
